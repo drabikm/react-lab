9c7f6cd0d3ce871a1ff65a4f35b158262769c9c7
Monika Drabik <monika@MacBook-Pro-Monika.local>
2026-01-17 13:01:40 +0100

    i dont kn ow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import "milligram";
+import MovieForm from "./MovieForm";
 
 function App() {
+  const [title, setTitle] = useState('Titanic');
+  const movies = [{title: "Django Unchained"}, {title: "Titanic"}, {title: "Notebook"}];
+  //const [movies, setMovies] = useState([]);
+
+  let message;
+
+  function handleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  if (title.length < 7) {
+    message = "za krotki, musisz cos dopisac"
+  } else if (title.length < 15) {
+    message = "Brawo Ty!"
+  } else {
+    message = "za dlugi, nie zadziala :D"
+  }
+
+  function a() {
+    alert.message("No i masz {title}")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="container">
+      <h1>My Favourite Movies :)</h1>
+      <ul>
+        {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+      </ul>
+      {
+        title.length > 0 && <div>{message}</div>
+      }
+      <input type="text" value={title} onChange={handleChange}></input>
+      <button tyep="button" onClick={() => alert(title)}>Pokaz no mi tytul</button>
+      <h2>Time to add new movie!</h2>
+
     </div>
   );
 }
