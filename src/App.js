d44239611030a233675cf162f3379b6c105d04d4
Monika Drabik <monika@MacBook-Pro-Monika.local>
2026-01-17 14:11:45 +0100

    adding movie works

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import MovieForm from "./MovieForm";
 
 function App() {
   const [title, setTitle] = useState('Titanic');
-  const movies = [{title: "Django Unchained"}, {title: "Titanic"}, {title: "Notebook"}];
-  //const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([
+    {title: "Django Unchained", year: 1999}, 
+    {title: "Titanic", year: "1987"}, 
+    {title: "Notebook", year: "1765"}]);
+  const [title_to_add, setTitle_to_add] = useState('');
+  const [year, setYear] = useState('');
 
   let message;
 
@@ -23,23 +27,30 @@ function App() {
     message = "za dlugi, nie zadziala :D"
   }
 
-  function a() {
-    alert.message("No i masz {title}")
+  function addMovie(event) {
+    event.preventDefault();
+    setMovies([...movies, {title, year}]);
   }
 
   return (
-    <div className="App" class="container">
+    <div className="App" className="container">
       <h1>My Favourite Movies :)</h1>
       <ul>
-        {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+        {movies.map((movie) => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
       </ul>
       {
         title.length > 0 && <div>{message}</div>
       }
       <input type="text" value={title} onChange={handleChange}></input>
-      <button tyep="button" onClick={() => alert(title)}>Pokaz no mi tytul</button>
+      <button type="button" onClick={() => alert(title)}>Pokaz no mi tytul</button>
       <h2>Time to add new movie!</h2>
-
+      <form onSubmit={(event) => addMovie(event) }>
+        <div>Wpisz prosze tytul filmu</div>
+        <input type="text" value={title_to_add} onChange={(event) => setTitle_to_add(event.target.value)}></input>
+        <div>... i nastepnie rok produkcji (nie musi byc prawdziwa)</div>
+        <input type="text" value={year} onChange={(event) => setYear(event.target.value)}></input>
+        <button>Dodaj film</button>
+      </form>
     </div>
   );
 }
